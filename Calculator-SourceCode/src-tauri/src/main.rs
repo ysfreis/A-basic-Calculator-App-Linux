830171b1dbc1fe6189b3530fdb4967d67a6de1e4
ysfreis <93714403+ysfreis@users.noreply.github.com>
2023-09-08 09:38:35 +0300

    Calculator Source code
    Happy code

---
+++ Calculator-SourceCode/src-tauri/src/main.rs
@@ -0,0 +1,38 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+#[tauri::command]
+fn hesapla(a: &str,b: &str,c: &str) -> String {
+    let sayi = match a.parse::<f64>() {
+        Ok(deger) => deger,
+        Err(_) => return format!("Hatalı giriş: {} veya {}", a, b),
+    };
+    let sayi2 = match c.parse::<f64>() {
+        Ok(deger) => deger,
+        Err(_) => return format!("Hatalı giriş: {} veya {}", a, b),
+    };
+    let sonuc = if b=="+"{
+        sayi+sayi2
+    }else if b=="-"{
+        sayi-sayi2
+         
+    }else if b=="/"{
+        sayi/sayi2
+    }else{
+        sayi*sayi2
+
+    };
+    
+    println!("{}",sonuc);
+    
+    format!("Yapılan İşlem:{}{}{}       Sonuç:{} ", a,b,c,sonuc)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![hesapla])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
