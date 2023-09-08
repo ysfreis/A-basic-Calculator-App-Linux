dd0ec071d7f132606d8a209bc17a47f4960de9fb
ysfreis <93714403+ysfreis@users.noreply.github.com>
2023-09-08 10:23:51 +0300

    Update main.rs

--- Calculator-SourceCode/src-tauri/src/main.rs
+++ Calculator-SourceCode/src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 #[tauri::command]
 fn hesapla(a: &str,b: &str,c: &str) -> String {
